93ff02fa833d327e2a0916aaa6acce3ff1c54adb
Simon Lee <simon.lee@cardinalblue.com>
2025-12-31 23:45:03 +0800

    Support live YouTube URLs

--- utils.ts
+++ utils.ts
@@ -1,7 +1,7 @@
 export const getYouTubeVideoId = (url: string): string | null => {
   if (!url) return null;
   
-  const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+  const regExp = /^.*(youtu.be\/|live\/|shorts\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
   const match = url.match(regExp);
 
   return (match && match[2].length === 11) ? match[2] : null;
